77008bb75ebfb8c99d19d96e871de4c23326d1aa
Ryaan Ahmed <ryaan.ahmed@gmail.com>
2019-03-12 15:29:43 +0000

    deal with spaces in filename

--- server.js
+++ server.js
@@ -163,14 +163,16 @@ app.post('/api/image_query', function(req, res) {
     let user_image = req.files.user_image_file;
 
     working_path = './run/';
+    const new_filename = user_image.name.replace(/ /g, '_');
+
 
     // Use the mv() method to save the file there
-    user_image.mv(working_path + user_image.name, (err) => {
+    user_image.mv(working_path + new_filename, (err) => {
         if (err) { return res.status(500).send(err); }
         else {
             console.log("Uploaded file saved as " + working_path + user_image.name);
             const ngram_search = false; // TODO(ra): make this work!
-            const result = run_image_query(user_image, ngram_search);
+            const result = run_image_query(new_filename, ngram_search);
             res.send(result);
         }
     });
@@ -238,7 +240,7 @@ function search_with_code(str, jaccard, num_results) {
 }
 
 
-function run_image_query(user_image, ngram_search) {
+function run_image_query(user_image_filename, ngram_search) {
     const jaccard = true; // TODO(ra) should probably get this setting through the POST request, too...
     const num_results = 20; // TODO(ra) should probably get this setting through the POST request, too...
 
@@ -247,12 +249,12 @@ function run_image_query(user_image, ngram_search) {
     let result;
 
     if(!ngram_search) {
-        query_data = cp.execSync('./shell_scripts/image_to_maws.sh ' + user_image.name + ' ' + working_path);
+        query_data = cp.execSync('./shell_scripts/image_to_maws.sh ' + user_image_filename + ' ' + working_path);
         query = String(query_data); // a string of maws, preceded with an id
         result = search('words', query, jaccard, num_results);
     }
     else {
-        query_data = cp.execSync('./shell_scripts/image_to_ngrams.sh ' + user_image.name + ' ' + working_path + ' ' + '9');
+        query_data = cp.execSync('./shell_scripts/image_to_ngrams.sh ' + user_image_filename + ' ' + working_path + ' ' + '9');
         query = String(query_data);
         result = search('words', query, jaccard, num_results, true);
     }

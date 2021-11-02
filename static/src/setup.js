1f9bfb5c104546967a077cfe934a77144504ade8
Alastair Porter <alastair@porter.net.nz>
2021-11-02 17:48:42 +0100

    Load examples

--- static/src/setup.js
+++ static/src/setup.js
@@ -727,12 +727,11 @@ function clear_result_divs() {
  * Browsing EMO
  ******************************************************************************/
 
-function show_example(example_id) {
-    load_page_query(example_id);
-    update_colls_to_search();
-    var search_num_results = parseInt(document.getElementById("res_disp_select").value);
-    search(example_id, jaccard, search_num_results, collections_to_search);
+function show_example(example) {
+    load_page_query(example.id);
+    current_page = {library: example.library, book: example.book, page: example.id};
     $('#examples_div').collapse('hide');
+    do_search();
 }
 
 /*******************************************************************************
@@ -873,33 +872,35 @@ function do_search() {
 
 function add_examples_list() {
     const examples = [
-        ['GB-Lbl_K2h7_092_1', "Different editions of Berchem, 'O s'io potessi donna' (<i>Cantus</i>)"],
-        ['GB-Lbl_A360a_005_0', "Very different editions of Striggio, 'Alma reale' (<i>Canto</i>)"],
-        ['GB-Lbl_K3k19_012_1', "Lassus, 'Susanna faire' (<i>Cantus</i>) and the original French chanson"],
-        ['GB-Lbl_K3k19_014_0', "Marenzio, 'I must depart all haples' (<i>Cantus</i>), and: (a) the original Italian madrigal; (b) the <i>Quinto</i> part of the latter. (The English <i>Quintus</i> part is ranked no. 9)"],
-        ['GB-Lbl_A324c_048_1', "Nanino, 'Morir non puo'l mio core' (<i>Alto</i>) and the English version (<i>Contratenor</i>) - note the two extra notes at the beginning"],
-        ['GB-Lbl_K3k12_010_0', "Marenzio, 'Sweet hart arise' (<i>Superius</i>), and the English version (<i>Canto</i>); the Italian <i>Quinto</i> part is ranked at 3 and the English <i>Medius</i> at 8"],
-        ['GB-Lbl_B270b_035_1', "Marenzio, 'Dhe se potessi' (<i>Basso</i>), and its <i>Tenor</i> part at rank 3; the <i>Cantus</i> part is at rank 5"],
-        ['GB-Lbl_K9a10_023_0', "Morales, 'Magnificat Sexti toni' (choirbook); ranks 2 & 3 are different voice-parts from the work"],
-        ['GB-Lbl_A19_004_0', "End of Clemens non Papa, 'Pater peccavi' and beginning of its <i>Secunda pars</i>, 'Quanti mercanarii' (<i>Tenor</i>); 'Pater peccavi' is at rank 2"],
-        ['GB-Lbl_K3e1_061_1', "Clemens non Papa, 'Angelus domini' (<i>Bassus</i>); another edition at rank 2; <i>Tenor</i> part at rank 3; another edition of <i>Bassus</i> at rank 5"],
-        ['GB-Lbl_K2a4_072_1', "Lassus, Psalm 11, 'Pourquoy font bruit' (<i>Contratenor</i>), and the chanson on which it is based, 'Las me faut', ranked at 2; at ranks 3 & 4 are the two pages of another edition of the chanson"],
-        ['GB-Lbl_A569c_024_0', "Willaert, 'Recercar quinto', was also published in a transposed version  (GB-Lbl_K3b4_013_0) as well as at the original pitch (GB-Lbl_K3b4_020_0)"],
-        ['GB-Lbl_K8f10_134_1', "Anonymous <i>lauda</i>, 'Ecco care sorelle' (<i>Cantus</i> and <i>Tenor</i> parts on same page!) is actually a close version of Verdelot, 'Fedel' e bel cagnuolo' (<i>Cantus</i> at rank 2; <i>Tenor</i> at rank 3)"],
-        ['GB-Lbl_A569c_013_1', "'Recercar undecimo' (<i>Canto</i>), by <i>Incerto Autore</i>; at rank 2 is Damianus, 'In die tribulationis' (scholars disagree about the identity of this composer); <i>Basso</i> part of the recercar at rank 3"],
-        ['D-Bsb_Parangon_03_1543_inv_060_0', "Arcadelt, 'Vous perdez temps' (Tenor); turns out to be musically identical to his madrigal, 'Non ch'io, non voglio' (K2h3_031_1)"],
+        {library: 'GB-Lbl', book: 'K2h7', id: 'GB-Lbl_K2h7_092_1', desc: "Different editions of Berchem, 'O s'io potessi donna' (<i>Cantus</i>)"},
+        {library: 'GB-Lbl', book: 'A360a', id: 'GB-Lbl_A360a_005_0', desc: "Very different editions of Striggio, 'Alma reale' (<i>Canto</i>)"},
+        {library: 'GB-Lbl', book: 'K3k19', id: 'GB-Lbl_K3k19_012_1', desc: "Lassus, 'Susanna faire' (<i>Cantus</i>) and the original French chanson"},
+        {library: 'GB-Lbl', book: 'K3k19', id: 'GB-Lbl_K3k19_014_0', desc: "Marenzio, 'I must depart all haples' (<i>Cantus</i>), and: (a) the original Italian madrigal; (b) the <i>Quinto</i> part of the latter, split between nos. 13 and 14"},
+        {library: 'GB-Lbl', book: 'A324', id: 'GB-Lbl_A324c_048_1', desc: "Nanino, 'Morir non puo'l mio core' (<i>Alto</i>) and the English version (<i>Contratenor</i>) - note the two extra notes at the beginning! There is also a Latin contrafactum at no. 6"},
+        {library: 'GB-Lbl', book: 'K3k12', id: 'GB-Lbl_K3k12_010_0',  desc: "Marenzio, 'Sweet hart arise' (<i>Superius</i>), and the English version (<i>Canto</i>); the Italian <i>Quinto</i> part is ranked at 15 - you can increase the number of results using the dropdown."},
+        {library: 'GB-Lbl', book: 'K9a10', id: 'GB-Lbl_K9a10_023_0', desc: "Morales, 'Magnificat Sexti toni' (choirbook); ranks 2 & 3 are different voice-parts from the work"},
+        {library: 'GB-Lbl', book: 'A19', id: 'GB-Lbl_A19_004_0',    desc: "End of Clemens non Papa, 'Pater peccavi' and beginning of its <i>Secunda pars</i>, 'Quanti mercanarii' (<i>Tenor</i>); 'Pater peccavi' is at rank 2"},
+        {library: 'GB-Lbl', book: 'K3e1', id: 'GB-Lbl_K3e1_061_1',  desc: "Clemens non Papa, 'Angelus domini' (<i>Bassus</i>); other editions at ranks 2 to 6, and 9; <i>Tenor</i> part at ranks 7 to 8, and 10; another edition of <i>Bassus</i> at rank 5"},
+        {library: 'GB-Lbl', book: 'K2a4', id: 'GB-Lbl_K2a4_072_1',  desc: "Lassus, Psalm 11, 'Pourquoy font bruit' (<i>Contratenor</i>), and the chanson on which it is based, 'Las me faut', ranked at 2; at ranks 3 & 4 are the two pages of another edition of the chanson"},
+        {library: 'GB-Lbl', book: 'A569c', id: 'GB-Lbl_A569c_024_0', desc: "Willaert, 'Recercar quinto', was also published in a transposed version  (GB-Lbl_K3b4_013_0) as well as at the original pitch (GB-Lbl_K3b4_020_0)"},
+        {library: 'GB-Lbl', book: 'K8f10', id: 'GB-Lbl_K8f10_134_1', desc: "Anonymous <i>lauda</i>, 'Ecco care sorelle' (<i>Cantus</i> and <i>Tenor</i> parts on same page!) is actually a close version of Verdelot, 'Fedel' e bel cagnuolo' (<i>Cantus</i> at rank 2; <i>Tenor</i> at rank 3)"},
+        {library: 'GB-Lbl', book: 'A569c', id: 'GB-Lbl_A569c_013_1', desc: "'Recercar undecimo' (<i>Canto</i>), by <i>Incerto Autore</i>; at rank 2 is Damianus, 'In die tribulationis' (scholars disagree about the identity of this composer); <i>Basso</i> part of the recercar at rank 3"},
+        {library: 'D-Bsb', book: 'Parangon_03', id: 'D-Bsb_Parangon_03_1543_inv_060_0', desc: "Arcadelt, 'Vous perdez temps' (Tenor); turns out to be musically identical to his madrigal, 'Non ch'io, non voglio' (K2h3_031_1)"},
     ];
 
-    const $examples_table = $('#examples_table');
+    const examples_table = $('#examples_table');
     for (const example of examples) {
-        const [id, note] = example;
-        const $row = $('<tr></tr>');
-        $examples_table.append($row);
-        const id_link = `<a href="#" onclick='show_example("${id}")'>View</a>`;
-        const $id_cell = $('<td>' + id_link + '</td>');
-        $row.append($id_cell);
-        const $note_cell = $('<td>' + note + '</td>');
-        $row.append($note_cell);
+        const row = $('<tr></tr>');
+        examples_table.append(row);
+        const exampleid = "example" + example.id;
+        const id_link = `<a href="#" id=${exampleid}>View</a>`;
+        const id_cell = $('<td>' + id_link + '</td>');
+        row.append(id_cell);
+        const note_cell = $('<td>' + example.desc + '</td>');
+        row.append(note_cell);
+        $("#"+exampleid).click(() => {
+            show_example(example);
+        });
     }
 }
 

35e11e24d7f67c711a361831c56c416c225e9fa9
Alastair Porter <alastair@porter.net.nz>
2022-04-11 17:40:55 +0200

    Add subsequence client search script

---
+++ client/subsequence.js
@@ -0,0 +1,70 @@
+import fetch from "node-fetch";
+
+const args = process.argv.slice(2);
+if (args.length < 2) {
+    console.error(`usage: ${process.argv[1]} [notes|intervals] [search-query] [num-results]`);
+    process.exit();
+}
+
+const type = args[0];
+const validTypes = ["notes", "intervals"];
+if (!validTypes.includes(type)) {
+    console.error("type must be 'notes' or 'intervals'");
+    process.exit();
+}
+
+const intervals = type === "intervals";
+const data = {interval: intervals, subsequence: args[1]};
+const query_length = args[1].split(" ").length;
+if (args.length > 2) {
+    data.num_results = args[2];
+}
+
+fetch("https://solrdev.f-tempo.org/api/query_subsequence", {
+    method: "POST",
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data)
+}
+).then(r => {
+    return r.json();
+}).then(data => {
+    console.log(data);
+    processResults(data, intervals, query_length);
+}).catch((error) => {
+    console.error('Error:', error);
+});
+
+// \t${Number.parseFloat(row.jaccard).toFixed(2)}
+
+function processResults(results, is_interval, query_length) {
+    if (results.status === "ok") {
+        results.data.results.forEach((row, index) => {
+            console.log(`${index + 1}\t${row.id}`);
+            
+            const codestring = is_interval ? row.codestring_intervals : row.codestring_notes;
+            // Assume that the match positions are non-overlapping. Draw [ ... ] boxes around each match
+            // and highlight them
+            const matchPositions = row.matches.map(match => match.start_position);
+            const reverseMatchPositions = [...matchPositions].reverse();
+            reverseMatchPositions.forEach(m => {
+                codestring.splice(m + query_length, 0, ']\x1b[0m');
+                codestring.splice(m, 0, '\x1b[33m[');
+            });
+            console.log(`\t${row.num_matches} matches at ${matchPositions.join(", ")}`);
+            console.log("\t" + codestring.join(" "));
+            // Alternatively, print a line for each match, highlighting the section where it occurs
+            // row.matches.forEach((match, matchindex) => {
+            //     const matchCodestring = [...codestring];
+            //     matchCodestring.splice(match.start_position + query_length, 0, ']\x1b[0m');
+            //     matchCodestring.splice(match.start_position, 0, '\x1b[33m[');
+            //     console.log(`\t${matchindex+1}. Starting at position ${match.start_position}`);
+            //     console.log("\t" + matchCodestring.join(" "));
+            // })
+            console.log("");
+        });
+    } else if (results.status === "error") {
+        console.error(`Error: ${results.error}`);
+    }
+}
